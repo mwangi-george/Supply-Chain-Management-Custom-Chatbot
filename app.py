0dbdd12f315a90607c9e2d71b0c4df4a12ba4ffa
mwangi-george <mwangigeorge648@gmail.com>
2024-03-20 13:54:29 +0300

    updated message preholder

--- app.py
+++ app.py
@@ -48,7 +48,7 @@ for message in st.session_state.messages:
             st.markdown(message["content"])
 
 # Accept user input
-if prompt := st.chat_input("Hi there! How can I help you today? Ask me anything related to SCM"):
+if prompt := st.chat_input("Hi there! How can I help you today?"):
     # Add user message to chat history
     st.session_state.messages.append({"role": "user", "content": prompt})
     # Display user message in chat message container
